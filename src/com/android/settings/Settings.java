533dabed3f3f6ccf756989fd3fb80b1a03a005fd
Jason Edson <jaysonedson@gmail.com>
2020-02-14 19:46:36 -0800

    Settings: Add current BenzoExtras categories
    Change-Id: I164751f2f8a0d36475bca43f0cd52f5ef8cf5616
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -247,4 +247,6 @@ public class Settings extends SettingsActivity {
      * Activity for Benzo Extras
      */
     public static class BenzoSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QuickSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ButtonSettingsActivity extends SettingsActivity { /* empty */ }
 }
