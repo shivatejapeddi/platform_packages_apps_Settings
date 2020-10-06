BenzoRom/packages_apps_Settings

Settings: Live Volume Steps [2/3]
Change-Id: I1cf0d9e5933a846600a723cb7720d47d3bd2d22a
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.benzo.settings.BenzoSettings;
 import com.benzo.settings.GlobalActionsSettings;
+import com.benzo.settings.VolumeStepsFragment;
 
 public class SettingsGateway {
 
@@ -288,7 +289,8 @@ public class SettingsGateway {
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
             BenzoSettings.class.getName(),
-            GlobalActionsSettings.class.getName()
+            GlobalActionsSettings.class.getName(),
+            VolumeStepsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -329,5 +331,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.BenzoSettingsActivity.class.getName(),
             Settings.GlobalActionsSettingsActivity.class.getName(),
+            Settings.VolumeStepsFragmentActivity.class.getName(),
     };
 }

shivatejapeddi/platform_packages_apps_Settings

Signed-off-by: Jason Edson <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -163,6 +163,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.benzo.settings.BenzoSettings;
 import com.benzo.settings.ButtonSettings;
 import com.benzo.settings.QuickSettings;
+import com.benzo.settings.VolumeStepsFragment;
 
 public class SettingsGateway {
 
@@ -314,7 +315,8 @@ public class SettingsGateway {
             MediaControlsSettings.class.getName(),
             BenzoSettings.class.getName(),
             QuickSettings.class.getName(),
-            ButtonSettings.class.getName()
+            ButtonSettings.class.getName(),
+            VolumeStepsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -358,5 +360,6 @@ public class SettingsGateway {
             Settings.BenzoSettingsActivity.class.getName(),
             Settings.QuickSettingsActivity.class.getName(),
             Settings.ButtonSettingsActivity.class.getName(),
+            Settings.VolumeStepsFragmentActivity.class.getName(),
     };
 }